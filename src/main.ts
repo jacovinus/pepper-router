2f0de6c89219912642a130337474086c6856abbc
Joel Guerra <jacovinus@gmail.com>
2024-06-20 15:29:04 +0200

    fix: test workflow run

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import routes from "./router/routes";
 import { simpleLogger } from "./router/util";
 
 routes.init(routes.server as Server, 3000, (req, res) => {
+    console.log(res)
     simpleLogger(req, res);
 });
-// the main component
+
